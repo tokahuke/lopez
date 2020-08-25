898b8f9f8793bf5f3544ec8b79aeb94db5d73d80
Pedro Arruda <pedrobittencourt3@protonmail.ch>
2020-08-25 09:11:41 -0300

    trying jemalloc; it might have benefits...

--- lopez/src/main.rs
+++ lopez/src/main.rs
@@ -1,3 +1,7 @@
 //! A whole crate for a line (and a nice name).
 
+/// Trying to see if I can get fragmentation reduction using jemalloc.
+#[global_allocator]
+static ALLOCATOR: jemallcator::Jemalloc = jemallcator::Jemalloc;
+
 lib_lopez::main! { postgres_lopez::PostgresBackend }
