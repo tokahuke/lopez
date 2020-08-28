59e83bec4a4232139345b3d8d5dd47eda6b6f392
Pedro Arruda <pedrobittencourt3@protonmail.ch>
2020-08-28 09:44:25 -0300

    updated editor support

--- lib-lopez/src/error.rs
+++ lib-lopez/src/error.rs
@@ -21,7 +21,7 @@ pub enum Error {
     Timeout,
     #[fail(display = "bad set-variable value for {}: {:?}", _0, _1)]
     BadSetVariableValue(crate::directives::Variable, crate::directives::Literal),
-    #[fail(display = "type error: no type for {} of {}", _0, _1)]
+    #[fail(display = "type error: no type for `{}` of `{}`", _0, _1)]
     TypeError(crate::directives::Transformer, crate::directives::Type),
     #[fail(display = "{}", _0)]
     Custom(String),
