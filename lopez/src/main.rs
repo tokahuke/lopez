0ac9e55dbd4095780fed40f83aa7f8000aa8d0c5
Pedro Arruda <pedrobittencourt3@protonmail.ch>
2020-09-21 23:16:55 -0300

    configurable logging

--- lopez/src/main.rs
+++ lopez/src/main.rs
@@ -1,6 +1,7 @@
 //! A whole crate for a line (and a nice name).
 
 /// Trying to see if I can get fragmentation reduction using jemalloc.
+#[cfg(not(target_env = "musl"))]
 #[global_allocator]
 static ALLOCATOR: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
